01b7998f9698a1312e01ddba4a97fca9dfaf0cb5
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2024-03-20 23:05:06 -0300

    structure folders

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { NativeBaseProvider, Text, Box, Heading } from "native-base";
-import * as Animatable from 'react-native-animatable';
-import { MaterialIcons } from '@expo/vector-icons';
+import SplashScreen from "./src/screens/splashScreen";
+import { NativeBaseProvider } from "native-base";
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-        <Animatable.View animation="fadeIn" duration={4000}>
-          <Heading color="primary.500" size='3xl'> motoGo</Heading>
-          <Box mt={4} justifyContent="center" alignItems="center" >
-            <MaterialIcons name="sports-motorsports" size={64} color="#06b6d4" />
-          </Box>
-        </Animatable.View>
-      </Box>
+      <SplashScreen />
     </NativeBaseProvider>
   );
 }
