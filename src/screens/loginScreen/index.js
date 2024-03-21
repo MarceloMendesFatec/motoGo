c7de65a41e4c40a5781c327a27eeb0b1083f111d
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2024-03-21 09:45:05 -0300

    adicionado links

--- src/screens/loginScreen/index.js
+++ src/screens/loginScreen/index.js
@@ -1,40 +1,58 @@
-
 import React from "react";
-import { NativeBaseProvider, Text, Box, Heading, Input, Button, Link, Center } from "native-base";
-import { MaterialIcons } from '@expo/vector-icons';
+import {
+    NativeBaseProvider,
+    Text,
+    Box,
+    Heading,
+    Input,
+    Button,
+    Link,
+    Center,
+} from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
 
-
-
-const LoginScreen = ({navigation}) => {//todas as telas recebem o parâmetro navigation
+const LoginScreen = ({ navigation }) => {
+    //todas as telas recebem o parâmetro navigation
     return (
         <NativeBaseProvider>
+            {/* //box central */}
             <Box p={4} mt={10} flex={1} justifyContent="space-around">
+                {/* //logo */}
                 <Center>
-                    <Heading color="primary.500" size='4xl' >motoGo</Heading>
+                    <Heading color="primary.500" size="4xl">
+                        motoGo
+                    </Heading>
                     <MaterialIcons name="sports-motorsports" size={64} color="#06b6d4" />
                 </Center>
-              <Box>
-                <Box mt={4}>
-                        <Input placeholder="Username" />
+                {/* //box do formulario */}
+                <Box>
+                    <Box mt={4}>
+                        <Input placeholder="Usuário" />
                     </Box>
                     <Box mt={4}>
-                        <Input placeholder="Password" type="password" />
+                        <Input placeholder="Senha" type="password" />
                     </Box>
                     <Box mt={4}>
-                        <Link alignSelf="flex-end" _text={{ color: "blue.500" }} onPress={() => {}}>
-                            Forgot Password?
+                        <Link alignSelf="flex-end" _text={{ color: "info.700" }} onPress={() => { }}>
+                            Esqueceu a senha?
                         </Link>
                     </Box>
                     <Box mt={4}>
-                        <Button colorScheme="primary" onPress={() => {}}>
-                            Login
+                        <Button shadow="2" onPress={() => { }} _text={{ fontSize: 18, fontWeight: "bold" }}>
+                            Entrar
                         </Button>
+                        <Link justifyContent='center' mt={4} _text={{ fontSize: "sm", fontWeight: "bold", color: "info.700" }} onPress={() => { }}>
+                            Não tem uma conta? Cadastre-se
+                        </Link>
                     </Box>
-              </Box>
-               
+
+                </Box>
+
+                {/* box formulario */}
             </Box>
+
         </NativeBaseProvider>
     );
-}
+};
 
 export default LoginScreen;
