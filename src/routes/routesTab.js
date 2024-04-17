7127870d71f997a9223ad7997bd49dc9a23d22dd
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2024-04-17 15:23:53 -0300

    foco  e cores na tab navigation

--- src/routes/routesTab.js
+++ src/routes/routesTab.js
@@ -1,19 +1,51 @@
 import React from "react";
 import { NativeBaseProvider, Text } from "native-base";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from "@expo/vector-icons";
 import UserScreen from "../screens/userScreen";
-import RateScreen from "../screens/rateScreen";
+import HistoricalScreen from "../screens/historicalScreen";
 import HomeScreen from "../screens/homeScreen";
 
 const Tab = createBottomTabNavigator();
 
-const AppTabNavigator = ({navigation}) => {
+const AppTabNavigator = ({ navigation }) => {
     return (
         <NativeBaseProvider>
             <Tab.Navigator>
-                <Tab.Screen name="Inicio" component={HomeScreen} options={{ headerShown: false}}/>
-                <Tab.Screen name="Usuario" component={UserScreen} options={{ headerShown: false}}/>
-                <Tab.Screen name="Avaliar" component={RateScreen} options={{ headerShown: false}} />
+                <Tab.Screen
+                    name="Inicio"
+                    component={HomeScreen}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({ focused }) => {
+                            return <MaterialIcons name={focused ? "home" : "home"} size={44} color={focused ? "#06b6d4" : "gray"} />;
+                        },
+                        tabBarShowLabel: false,
+                    }}
+                />
+
+                <Tab.Screen
+                    name="Historico"
+                    component={HistoricalScreen}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({focused}) => {
+                            return <MaterialIcons name={focused? "list" : "list"} size={44} color={focused ? "#06b6d4" : "gray"} />;
+                        },
+                        tabBarShowLabel: false,
+                    }}
+                />
+                <Tab.Screen
+                    name="Usuario"
+                    component={UserScreen}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({focused}) => {
+                            return <MaterialIcons name={focused? "person" : "person"} size={44} color={focused ? "#06b6d4" : "gray"} />;
+                        },
+                        tabBarShowLabel: false,
+                    }}
+                />
             </Tab.Navigator>
         </NativeBaseProvider>
     );
---
+++ src/screens/historicalScreen/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NativeBaseProvider, Text } from "native-base";
+
+const HistoricalScreen = ({ navigation }) => {
+    return (
+        <NativeBaseProvider>
+            <Text>HistoricalScreen</Text>
+        </NativeBaseProvider>
+    );
+}
+
+export default HistoricalScreen;
