81fa3094ae0147be05447b73070620290adb9e0b
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2024-04-23 17:58:31 -0300

    modal enviar email

--- src/screens/ForgotPasswordScreen/index.js
+++ src/screens/ForgotPasswordScreen/index.js
@@ -1,27 +1,34 @@
 import React, { useState } from 'react';
-import { Heading, NativeBaseProvider,Text } from "native-base";
-import { VStack, Input, Button, Center } from "native-base";
+import { Heading, NativeBaseProvider, Text } from "native-base";
+import { VStack, Input, Button, Center, Image, Modal } from "native-base";
+import { HStack, Icon } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
 
-
-const ForgotPasswordScreen = ({navigation}) => {
+const ForgotPasswordScreen = ({ navigation }) => {
     const [email, setEmail] = useState('');
-    
+    const [showModal, setShowModal] = useState(false);
+
+
     const sendPasswordReset = () => {
-        console.log('Sending password reset email to: ', email);
-        if(email === ''){
-            alert('Preencha o campo de email');
-        }else if(!/\S+@\S+\.\S+/.test(email)){
-            alert('Email inválido');
+        if (email === '') {
+            setShowModal(true);
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            setShowModal(true);
         };
+    };
 
-
+    const closeModal = () => {
+        setShowModal(false);
     };
 
     return (
         <NativeBaseProvider>
-            
-            <VStack space={10} alignItems="center" p={5} mt={10}>
-                <Heading size="xl" color="primary.500">Enviar link de redefinição </Heading>
+            <VStack space={10} alignItems="center" p={5} mt={20}>
+                <Heading size="xl" color="primary.500">Esqueceu a sua senha?  </Heading>
+                <Image source={require('../../assets/Forgot-password.png')} style={{ width: 200, height: 200 }} alt='imagem de esqueceu a senha' />
+                <Center>
+                    <Text fontSize="md">Não se preocupe ! Insira o seu email cadastrado para receber um link para redefinir a sua senha.</Text>
+                </Center>
                 <Input
                     placeholder="Email"
                     value={email}
@@ -31,7 +38,26 @@ const ForgotPasswordScreen = ({navigation}) => {
                     Enviar
                 </Button>
             </VStack>
-          
+
+
+            {showModal && (
+                <Modal isOpen={showModal} onClose={closeModal}>
+                    <Modal.Content>
+
+                        <HStack justifyContent="center" alignItems="center" m={4}>
+                            <Icon as={MaterialIcons} name="warning" size={12} color="danger.500" mr={2} />
+                            <Text color="danger.500" fontWeight="bold" fontSize="xl">Atenção!</Text>
+                        </HStack>
+                        <Text color="gray.600" textAlign="center" fontSize="md">O campo do email não pode estar em branco ou ser um email inválido.</Text>
+                        <Button onPress={closeModal} m={4} alignSelf="center" backgroundColor="danger.500">
+                            Entendido
+                        </Button>
+
+
+
+                    </Modal.Content>
+                </Modal>
+            )}
         </NativeBaseProvider>
     );
 };
