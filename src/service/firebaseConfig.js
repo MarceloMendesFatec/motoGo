65312d66c7cf4c2e79d847ed0fb318d3f5ac90dc
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2024-04-23 21:20:37 -0300

    validacoes de formulario  sem termos de compromisso OK

--- src/service/firebaseConfig.js
+++ src/service/firebaseConfig.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,5 +19,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+
 
 export default db;
